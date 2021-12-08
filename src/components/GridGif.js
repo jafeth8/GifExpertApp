aa81d351e219c8188f52e7c66427df6b3adf288b
Jafeth8 <romeojafet@gmail.com>
2021-12-08 00:57:30 -0600

    GiftExpertApp terminado

--- src/components/GridGif.js
+++ src/components/GridGif.js
@@ -1,32 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import { getGifs } from '../helpers/GetGifs';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+
 import { GifGridItem } from './GifGridItem';
 
 export const GridGif = ({category}) => {
 
-    const [images, setImages] = useState([])
-
-    useEffect(()=> {
-        //getGifs();
-        getGifs(category)
-            .then((img)=>{setImages(img)})
-    },[category])
-        
+    
+    const {data:images,loading}=useFetchGifs(category)
+    console.log("cargando componente");
+     
     return (
         <>
-            <h3>{category}</h3>
+            <h3 className="animate__animated animate__fadeIn">{category}</h3>
+            {loading?<p className="animate__animated animate__flash" >loading..</p>:null}
             <div className="card-grid">
-            {
-                images.map(img =>(
-                    <GifGridItem 
-                    key={img.id} 
-                    //img={img}
-                    {...img}
-                    />
-                ))
-            }
+            
+                {
+                    images.map(img =>(
+                        <GifGridItem 
+                        key={img.id} 
+                        //img={img}
+                        {...img}
+                        />
+                    ))
+                }
+            
             </div>
+
         </>
- 
+        
     )
 }
